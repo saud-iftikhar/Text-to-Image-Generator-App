5e19b1a17e599ec337e01fcc9a61b32c15136ea5
saudiftikhaar <saudiftikhar39@gmail.com>
2025-07-20 15:46:15 +0500

    second commit of changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import Description from '../components/Description'
+import GenerateBtn from '../components/GenerateBtn'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
+      <Description/>
+      <Testimonials/>
+      <GenerateBtn/>
     </div>
   )
 }
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { assets } from "../assets/assets"
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <form className="flex flex-col min-h-[90vh] justify-center items-center">
+    <div>
+      <div className="relative">
+        <img className="max-w-sm rounded" src={assets.sample_img_1} alt="" />
+        <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full 
+        transition-all duration-[10s]"/>
+      </div>
+      <p>Loading.....</p>
+    </div>
+
+    <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+       <input className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color" 
+       type="text" placeholder="Describe what you want to generate" />
+       <button className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full" type="submit">Generate</button>
+    </div>
+
+    </form>
   )
 }
 
