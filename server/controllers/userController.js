84ebe5bda50a8b1b11aa2c00bc11ba8abc98ef0b
saudiftikhaar <saudiftikhar39@gmail.com>
2025-07-25 02:57:02 +0500

    authentication and login functionality is completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
@@ -32,3 +32,43 @@ const registerUser = async (req, res) => {
    }
 
 }
+
+const loginUser = async (req, res) => {
+   try {
+      const {email, password} = req.body;
+      const user = await userModel.findOne({email})
+
+      if(!user){
+         return res.json({success:false, message: 'User does not exist'})
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if (isMatch) {
+         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+         res.json({success: true, token, user: {name: user.name}})
+
+      } else {
+         return res.json({success:false, message: 'Invalid Credentials'})
+      }
+
+   } catch (error) {
+     console.log(error)
+      res.json({success: false, message: error.message})  
+   }
+}
+
+const userCredits = async (req, res) => {
+   try {
+         const {userId} = req.body
+
+         const user = await userModel.findById(userId)
+         res.json({success: true, credits: user.creditBalance, user: {name: user.name}})
+   } catch (error) {
+         console.log(error.message)
+         res.json({success: false, message: error.message})
+   }
+}
+
+export {registerUser, loginUser, userCredits}
